3e4b11f681b029e47db7f221504bdc6e7a1525d0
foqc <fabiaq44@gmail.com>
2020-05-16 23:28:39 -0500

    refactoring mandelbrot set

--- mandelbrot.js
+++ mandelbrot.js
@@ -10,11 +10,17 @@ const WIDTH = 800
 const HEIGHT = 600
 ctx.canvas.width = WIDTH
 ctx.canvas.height = HEIGHT
-
+let worker
 const REAL_SET = { start: -2, end: 1 }
 const IMAGINARY_SET = { start: -1, end: 1 }
+let RS = math.subtract(REAL_SET.end, REAL_SET.start)
+const TASKS = []
+math.config({
+    number: 'BigNumber',
+    precision: 64
+})
 
-const colors = new Array(16).fill(0).map((_, i) => i === 0 ? '#000' : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
+let colors = new Array(16).fill(0).map((_, i) => i === 0 ? '#000' : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
 
 const valToHex = (rgb) => {
     let hex = Number(rgb).toString(16)
@@ -32,7 +38,7 @@ const myColors = () => {
     let colors = ['#000']
     const points = [[0, 0], [15, 255]]
     let color
-    for (let i = 0 i <= 15 i++) {
+    for (let i = 0; i <= 15; i++) {
         color = parseInt(lagrange(points[0], points[1], i))
         colors.push(rgbToHex(color, color, color))
     }
@@ -42,37 +48,44 @@ const myColors = () => {
     return a
 }
 
-const WORKERS = 1
-const myWorkers = new Array(WORKERS).fill(0).map(_ => new Worker('worker.js'))
-myWorkers.forEach(x => x.postMessage({ w: WIDTH, h: HEIGHT, rs: REAL_SET, is: IMAGINARY_SET, isSettingUp: true }))
+function start() {
+    for (let row = 0; row < WIDTH; row++) {
+        x = Number(math.add(math.bignumber(REAL_SET.start), math.multiply(RS, math.divide(math.bignumber(row), WIDTH))))
+        TASKS[row] = { x, row }
+    }
+    const task = TASKS.shift()
+    worker.postMessage({ x: task.x, row: task.row })
+}
 
-function draw() {
-    const p = parseInt(HEIGHT / WORKERS)
-    let k = 0, l = 0
-    const colors = myColors()
-    for (let i = 0 i < WIDTH i++) {
-        k = 0
-        for (let j = 0 j < HEIGHT j++, l++) {
-            myWorkers[k].postMessage({ i, j })
-            myWorkers[k].onmessage = e => {
-                const { mandelbrot, x, y } = e.data
-                const [m, isMandelbrotSet] = mandelbrot
-                ctx.fillStyle = colors[isMandelbrotSet ? 0 : (m % colors.length - 1) + 1]
-                ctx.fillRect(x, y, 1, 1)
-            }
-            if (l === p) {
-                k = k < WORKERS - 1 ? k + 1 : 0
-                l = 0
-            }
-        }
+function draw(res) {
+    if (TASKS.length > 0) {
+        const task = TASKS.shift()
+        worker.postMessage({ x: task.x, row: task.row })
+    }
+
+    const { row, mandelbrotSets } = res.data;
+    for (let i = 0; i < HEIGHT; i++) {
+        const [m, isMandelbrotSet] = mandelbrotSets[i]
+        ctx.fillStyle = colors[isMandelbrotSet ? 0 : (m % colors.length - 1) + 1]
+        ctx.fillRect(row, i, 1, 1)
     }
 }
 
-draw()
+function init() {
+    if (worker) worker.terminate()
+    worker = new Worker('worker.js')
+    worker.postMessage({ w: WIDTH, h: HEIGHT, rs: REAL_SET, is: IMAGINARY_SET, isSettingUp: true })
+    start()
+    colors = myColors()
+    worker.onmessage = draw
+}
+
+init()
 
 const ALFA = 100
 
 canvas.addEventListener('dblclick', e => {
+    worker.terminate()
     const pxStart = e.x - ALFA
     const pxEnd = e.x + ALFA
 
@@ -82,103 +95,5 @@ canvas.addEventListener('dblclick', e => {
     myWorkers.map(x => x.postMessage({ pxStart, pxEnd, pyStart, pyEnd, isResizing: true }))
     console.log('R -> ', REAL_SET.start, '  ', REAL_SET.end, ' I -> ', IMAGINARY_SET.start, '  ', IMAGINARY_SET.end)
 
-
-    draw()
+    init()
 })
-
-const terminate = () => {
-    myWorkers.map(x => x.terminate())
-}
-
-function createMouse(element) {
-    var mouse = {
-        x: 0,
-        y: 0,
-        button1: false,
-        button2: false,
-        button3: false,
-        over: false,
-    }
-    function mouseEvent(event) {
-        var bounds = element.getBoundingClientRect()
-
-        // NOTE getting the border should not be done like this as
-        // it will not work in all cases. 
-        var border = Number(element.style.border.split("px")[0])
-
-        mouse.x = event.pageX - bounds.left - scrollX - border
-        mouse.y = event.pageY - bounds.top - scrollY - border
-        if (event.type === "mousedown") {
-            mouse["button" + event.which] = true
-        } else if (event.type === "mouseup") {
-            mouse["button" + event.which] = false
-        } else if (event.type === "mouseover") {
-            mouse.over = true
-        } else if (event.type === "mouseout") {
-            mouse.over = false
-            mouse.button1 = false  // turn of buttons to prevent them locking
-            mouse.button2 = false
-            mouse.button3 = false
-        }
-        event.preventDefault() // stops default mouse behaviour.
-    }
-    var events = "mousemove,mousedown,mouseup,mouseout,mouseover".split(',')
-    events.forEach(eventType => element.addEventListener(eventType, mouseEvent))
-    mouse.remove = function () {
-        events.forEach(eventType => element.removeEventListener(eventType, mouseEvent))
-    }
-    return mouse
-}
-var drag = {
-    x: 0,
-    y: 0,
-    x1: 0,
-    y1: 0,
-    dragging: false,
-    top: 0,
-    left: 0,
-    width: 0,
-    height: 0,
-}
-var mouse = createMouse(canvas)
-function update() {
-    // ctx.clearRect(drag.left,  drag.top, drag.width, drag.height)
-    if (mouse.button1) {  // is button down
-        if (!drag.dragging) { // is dragging
-            drag.x = mouse.x
-            drag.y = mouse.y
-            drag.dragging = true
-        }
-        drag.x1 = mouse.x
-        drag.y1 = mouse.y
-        drag.top = Math.min(drag.y, drag.y1)
-        drag.left = Math.min(drag.x, drag.x1)
-        drag.width = Math.abs(drag.x - drag.x1)
-        drag.height = Math.abs(drag.y - drag.y1)
-    } else {
-        if (drag.dragging) {
-            console.log('is up ', drag)
-            myWorkers.map(x => x.postMessage({ pxStart: drag.left, pxEnd: drag.width, pyStart: drag.top, pyEnd: drag.height, isResizing: true }))
-            draw()
-            drag.dragging = false
-        }
-    }
-
-    if (drag.dragging) {
-        ctx.strokeRect(drag.left, drag.top, drag.width, drag.height)
-    }
-    requestAnimationFrame(update)
-}
-requestAnimationFrame(update)
-
-
-// const letters = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-// const width = 3;
-// const maxIndex = letters.length * width
-
-// for(let i = 0; i < maxIndex; i++) {
-//     let row = parseInt(i / width); // determines row
-//     let column = i % width; // determines column
-
-//     console.log("Value["+letters[row][column]+"] Row["+row+"] Column["+column+"]");
-// }
--- worker.js
+++ worker.js
@@ -5,46 +5,44 @@ math.config({
     precision: 64
 })
 
-let WIDTH, HEIGHT, REAL_SET, IMAGINARY_SET
+let WIDTH, HEIGHT, REAL_SET, IMAGINARY_SET, RS, IS
 const MAX_ITERATION = 1000
 onmessage = function (e) {
-    const { w, h, rs, is, i, j, isSettingUp, pxStart, pxEnd, pyStart, pyEnd, isResizing } = e.data
+    const { isSettingUp, isResizing } = e.data
     if (isSettingUp) {
-        WIDTH = w
-        HEIGHT = h
+        const { w, h, rs, is } = e.data
         REAL_SET = { start: math.bignumber(rs.start), end: math.bignumber(rs.end) }
         IMAGINARY_SET = { start: math.bignumber(is.start), end: math.bignumber(is.end) }
+        RS = math.subtract(REAL_SET.end, REAL_SET.start)
+        IS = math.subtract(IMAGINARY_SET.end, IMAGINARY_SET.start)
+        WIDTH = math.bignumber(w)
+        HEIGHT = math.bignumber(h)
     } else {
         if (isResizing) {
+            const { pxStart, pxEnd, pyStart, pyEnd } = e.data
             REAL_SET.start = getRelativePoint(pxStart, WIDTH, REAL_SET)
             REAL_SET.end = getRelativePoint(pxEnd, WIDTH, REAL_SET)
 
             IMAGINARY_SET.start = getRelativePoint(pyStart, HEIGHT, IMAGINARY_SET)
             IMAGINARY_SET.end = getRelativePoint(pyEnd, HEIGHT, IMAGINARY_SET)
 
-        } else
-            postMessage({ mandelbrot: main(i, j), x: i, y: j })
+        } else {
+            const { x, row } = e.data
+            const mandelbrotSets = []
+            for (let col = 0; col < HEIGHT; col++)
+                mandelbrotSets[col] = main(x, col)
+
+            postMessage({ row, mandelbrotSets })
+        }
     }
 }
 const main = (i, j) => mandelbrot(calc(i, j))
 
-const calc = (i, j) => {
-    i = math.bignumber(i)
-    j = math.bignumber(j)
-    w = math.bignumber(WIDTH)
-    h = math.bignumber(HEIGHT)
+const calc = (x, y) => {
+    y = math.bignumber(y)
+    y = math.add(IMAGINARY_SET.start, math.multiply(IS, math.divide(y, HEIGHT)))
 
-    rs = math.substract(REAL_SET.end, REAL_SET.start)
-    rd = math.divide(i, w)
-    rt = math.multiply(rs, rd)
-    x = math.add(REAL_SET.start, rt)
-
-    is = math.substract(IMAGINARY_SET.end, IMAGINARY_SET.start)
-    id = math.divide(j, h)
-    it = math.multiply(is, id)
-    y = math.add(IMAGINARY_SET.start, it)
-
-    return { x: math.number(x), y: math.number(y) }
+    return { x, y: math.number(y) }
 }
 
 function mandelbrot(c) {
@@ -64,12 +62,11 @@ function mandelbrot(c) {
     return [n, d <= 2]
 }
 
-const getRelativePoint = (pixel, length, set) => {
+const getRelativePoint = (pixel, l, set) => {
     p = math.bignumber(pixel)
-    l = math.bignumber(length)
 
     pl = math.divide(p, l)
-    es = math.substract(set.end, set.start)
+    es = math.subtract(set.end, set.start)
 
     t0 = math.add(pl, es)
     t = math.multiply(set.start, t0)
