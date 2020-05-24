e618a3d6b5a6abf31a416f947c4c8696a5e09402
foqc <fabiaq44@gmail.com>
2020-05-23 21:14:10 -0500

    refactor palette colors

--- mandelbrot.js
+++ mandelbrot.js
@@ -14,50 +14,46 @@ ctx.canvas.height = HEIGHT
 let worker
 let REAL_SET = { start: -2, end: 1 }
 let IMAGINARY_SET = { start: -1, end: 1 }
-const TASKS = []
 const ZOOM_FACTOR = 0.1
+const TASKS = []
 
 math.config({ number: 'BigNumber', precision: 64 })
 
 const lagrange = ([X1, Y1], [X2, Y2], x) => (((Y1 * (x - X2)) / (X1 - X2)) + ((Y2 * (x - X1)) / (X2 - X1)))
 
-const myColors = () => {
-    // rgb(255, 0, 0)
-    //rgb(255, 255, 0)
-    // rgb(0, 255, 0)
-    // rgb(0,255,255)
-    // rgb(0, 0, 255)
-    // rgb(255, 0, 255)
+const makeRGB = (r, g, b, k) => {
+    const calculate = pair => parseInt(lagrange(pair[0], pair[1], k))
+    if (isNaN(r)) r = calculate(r)
+    if (isNaN(g)) g = calculate(g)
+    if (isNaN(b)) b = calculate(b)
+
+    return [r, g, b]
+}
+
+/**
+ * get 250 colors from  interpolation (between 6 colors):
+ * rgb(255, 0, 0) -> rgb(255, 255, 0) -> rgb(0, 255, 0)
+ * rgb(0,255,255) -> rgb(0, 0, 255) -> rgb(255, 0, 255)
+ */
+const pelette = (size = 250) => {
+    const range = parseInt(size / 6)
     const colors = []
-    let r = 0, g = 0, b = 0
-    for (let k = 0; k < 250; k++) {
-        if (k <= 42) {//red to yellow => rgb(255, 0, 0) rgb(255, 255, 0) 
-            r = 255
-            g = parseInt(lagrange([1, 0], [42, 255], k))
-            b = 0
-        }
-        else if (k <= 84) {//yellow to green => rgb(255, 255, 0) rgb(0, 255, 0)
-            r = parseInt(lagrange([43, 255], [84, 0], k))
-            g = 255
-            b = 0
-        } else if (k <= 126) {//green to blue => rgb(0, 255, 0) rgb(0,255,255)
-            r = 0
-            g = 255
-            b = parseInt(lagrange([85, 0], [126, 255], k))
-        } else if (k <= 168) {//green to blue => rgb(0,255,255) rgb(0, 0, 255)
-            r = 0
-            g = parseInt(lagrange([127, 255], [168, 0], k))
-            b = 255
-        } else if (k <= 210) {//blue to red => rgb(0, 0, 255) rgb(255, 0, 255)
-            r = parseInt(lagrange([169, 0], [210, 255], k))
-            g = 0
-            b = 255
-        } else if (k < 250) {//blue to red => rgb(255, 0, 255) rgb(255, 0, 0)
-            r = 255
-            g = 0
-            b = parseInt(lagrange([211, 255], [249, 0], k))
-        }
-        colors.push([r, g, b])
+    let c
+    for (let k = 0; k < size; k++) {
+        if (k <= range)//red to yellow
+            c = makeRGB(255, [[0, 0], [range, 255]], 0, k)
+        else if (k <= range * 2)//yellow to green
+            c = makeRGB([[range + 1, 255], [range * 2, 0]], 255, 0, k)
+        else if (k <= range * 3)//green to cyan
+            c = makeRGB(0, 255, [[range * 2 + 1, 0], [range * 3, 255]], k)
+        else if (k <= range * 4)//cyan to blue
+            c = makeRGB(0, [[range * 3 + 1, 255], [range * 4, 0]], 255, k)
+        else if (k <= range * 5)//blue to purple
+            c = makeRGB([[range * 4 + 1, 0], [range * 5, 255]], 0, 255, k)
+        else//purple to red
+            c = makeRGB(255, 0, [[range * 5 + 1, 255], [size - 1, 0]], k)
+
+        colors.push(c)
     }
     return colors
 }
@@ -85,12 +81,11 @@ function init() {
     worker = new Worker('worker.js')
     worker.postMessage({ w: WIDTH, h: HEIGHT, rs: REAL_SET, is: IMAGINARY_SET, isSettingUp: true })
     start()
-    colors = myColors()
+    colors = pelette()
     worker.onmessage = draw
 }
 
 init()
-// const range = parseInt(250 / 6)
 
 canvas.addEventListener('dblclick', e => {
     const zfw = (WIDTH * ZOOM_FACTOR)
